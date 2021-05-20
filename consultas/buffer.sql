a625ee05b4ac6fa381fbbc50431a3d1c53d81729
deborafb <34282455+deborafb@users.noreply.github.com>
2021-05-20 09:51:40 -0300

    Update buffer.sql

--- consultas/buffer.sql
+++ consultas/buffer.sql
@@ -1,6 +1,6 @@
 --cria um buffer nas mesoregiões que tem um perimetro menor ou igual a 500. 
 
-select st_buffer(geom, 100, 'quad_segs=8') 
-from mesorregioes m
-where st_perimeter(m.geom) <= 500
+select st_buffer(geom, 5000, 'quad_segs=8')
+from escolas_estaduais
+where mun= 'SAO PAULO' 
 
