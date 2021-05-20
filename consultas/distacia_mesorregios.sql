e8131a602ef6d04e3d55c0e17a92ed8335f4ba5a
deborafb <34282455+deborafb@users.noreply.github.com>
2021-05-19 23:31:56 -0300

    Update distacia_mesorregios.sql

--- consultas/distacia_mesorregios.sql
+++ consultas/distacia_mesorregios.sql
@@ -1,3 +1,3 @@
 --Retorna a distância entre as mesorregiões (presidente prudente - ribeirão preto)
-select  st_distance (c1.geom, c2.geom) from mesoregioes c1, mesoregioes c2 
-where  c1.nome = 'PRESIDENTE PRUDENTE' and c2.nome = 'RIBEIRAO PRETO'
+select  st_distance (c1.geom, c2.geom) from mesorregioes c1, mesorregioes c2 
+where  c1.nm_meso = 'Presidente Prudente' and c2.nm_meso = 'Ribeirão Preto'
